9a577f366c1fa5441a9c734b63591a883dc49cc7
ldxldx <335.msly>
2017-07-28 17:34:47 +0800

    基础构建

--- server/server.js
+++ server/server.js
@@ -1,16 +1,36 @@
 const h = require('http');
-const MongoClient = require('mongodb').MongoClient;
-const DB_ADDRESS = 'mongodb://localhost:27017/';
-const server = h.createServer((req,res)=>{
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+let MongoClient = require('mongodb').MongoClient;
+let DB_CONN_STR = 'mongodb://localhost:27017/vip';
+
+const server = h.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('服务器创建成功！');
 }).listen(3000);
 let io = require('socket.io')(server);
-io.on('connection',function(socket){
+io.on('connection', function (socket) {
     //连接成功后
-    socket.on('sendMessage',function(data){
+    socket.on('sendMessage', function (data) {
         //接受到客户传过来数据
-        socket.broadcast.emit('newMessage',data);
+        socket.broadcast.emit('newMessage', data);
     });
 });
+MongoClient.connect(DB_CONN_STR, function(err, db) {
+
+    db.collection('vip').find().toArray(function (err,docs) {
+        console.log(docs);
+    })
+
+});
+// db.open((err, db) => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     db.collection('vip',(err,collection)=>{
+//         collection.find().toArray(function(err,docs){
+//             console.log('find');
+//             console.log(docs);
+//         });
+//     });
+// });
 
